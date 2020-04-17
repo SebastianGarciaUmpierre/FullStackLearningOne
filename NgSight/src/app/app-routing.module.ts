2fc600ed49b1693212d065cd1ed23f28e019131a
sebastiangarciaumpierre <sebastiangarciaumpierre@gmail.com>
2020-04-17 17:50:04 -0400

    Set up routes

--- NgSight/src/app/app-routing.module.ts
+++ NgSight/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sales', component: SectionSalesComponent},
+  {path:'orders', component: SectionOrdersComponent},
+  {path: 'health', component: SectionHealthComponent},
+  {path: '', component: SectionSalesComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [SectionSalesComponent, SectionOrdersComponent, SectionHealthComponent]
