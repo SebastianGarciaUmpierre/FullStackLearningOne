64f40ad68b84ecb470ddf52aaa3787f94ee8ccae
sebastiangarciaumpierre <sebastiangarciaumpierre@gmail.com>
2020-04-20 18:28:49 -0400

    Created sample data version of line chart

--- NgSight/src/app/charts/line-chart/line-chart.component.ts
+++ NgSight/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS} from '../../shared/chart.colors'
+
+
+
+const SAMPLE_LINECHART_DATA: any[] = [
+  {data: [6, 21, 43, 23, 14, 13, 50] , label: 'Computers'},
+  {data: [9, 23, 63, 13, 44, 33, 19] , label: 'Cameras'},
+  {data: [17, 12, 43, 63, 14, 33, 19] , label: 'Televisions'}
+] 
+
+const SAMPLE_LINECHART_LABELS: string[] = [ 'W1', 'W2', 'W3', 'W5', 'W6', 'W7']
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +19,16 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  public lineChartData: any[] = SAMPLE_LINECHART_DATA;
+  public lineChartLabels: string[] = SAMPLE_LINECHART_LABELS;
+  public lineChartType = 'line';
+  public lineChartLegend = true;
+  public lineChartOptions: any = {
+    responsive: true
+  }
+  public lineChartColors = LINE_CHART_COLORS;
+
 
   ngOnInit() {
   }
